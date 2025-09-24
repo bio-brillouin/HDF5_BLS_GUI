f062766ce9b563fb935d97cfc1024e3790981cb5
PierreBouvet <pierre.bouvet@meduniwien.ac.at>
2025-09-24 11:35:12 +0200

    Adding initial files to source

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from PySide6 import QtCore as qtc
+from PySide6 import QtWidgets as qtw
+from PySide6 import QtGui as qtg
+import time
+
+from Main import main
+
+if __name__ == "__main__":
+    app = qtw.QApplication(sys.argv)
+    main_win = main.MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
+    
